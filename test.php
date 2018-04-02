a93ce4d7e1e7d1d58975c7dea18c5aed929c0ce5
Dyb 3438 <503553364@qq.com>
2018-04-02 20:51:03 +0800

    modify the socket_server

---
+++ test.php
@@ -0,0 +1,23 @@
+<?php
+use Workerman\Worker;
+require_once __DIR__ . '/Workerman/Autoloader.php';
+
+
+$http_worker = new Worker("websocket://0.0.0.0:2000");
+
+$http_worker->count = 4;
+
+$userid=array();
+
+$http_worker->onConnect=function($connection){
+    global $userid;
+    if(!in_array($connection->getRemoteIp(),$userid)){
+        array_push($userid,$connection->getRemoteIp());
+    }
+    print_r($userid);
+};
+$http_worker->onMessage = function($connection, $data)
+{
+    $connection->send('hello '.$data);
+};
+Worker::runAll();
