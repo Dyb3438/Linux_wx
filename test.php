fd38a29f9458444d50d3aec7f20547c07549d6aa
Dyb 3438 <503553364@qq.com>
2018-04-05 23:25:55 +0800

    add libsvm

--- test.php
+++ test.php
@@ -1,7 +1,7 @@
 <?php
 use Workerman\Worker;
 require_once __DIR__ . '/Workerman/Autoloader.php';
-
+date_default_timezone_set("Asia/Shanghai");
 
 $http_worker = new Worker("websocket://0.0.0.0:2000");
 
@@ -10,14 +10,28 @@ $http_worker->count = 4;
 $userid=array();
 
 $http_worker->onConnect=function($connection){
-    global $userid;
-    if(!in_array($connection->id,$userid)){
-        array_push($userid,$connection->id);
-    }
-    print_r($userid);
+
 };
 $http_worker->onMessage = function($connection, $data)
 {
-    $connection->send('hello '.$data);
+    global $http_worker,$userid;
+    foreach($userid as $key=>$value){
+        if(!isset($http_worker->connections[$key])){
+            unset($userid[$key]);
+        }
+    }
+    if(!isset($userid[$connection->id])){
+        $data = explode(',', $data);
+        $userid[$connection->id]=array("id"=>$connection->id,"userid"=>$data[0],"name"=>$data[1]);
+    } else {
+        //此处为收到信息后的操作
+        $data=array("name"=>$userid[$connection->id]['name'],"image"=>$data,"time"=>date("Y-m-d H:i:s"));
+        $data=json_encode($data);
+        foreach($userid as $key=>$value){
+            $http_worker->connections[$key]->send($data);
+        }
+
+
+    }
 };
 Worker::runAll();
