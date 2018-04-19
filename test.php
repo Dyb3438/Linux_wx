823f36fa294f675f7719be36fcd24534c5208c63
Dyb 3438 <503553364@qq.com>
2018-04-19 16:41:22 +0800

    正在做预约系统接口

--- test.php
+++ test.php
@@ -7,31 +7,52 @@ $http_worker = new Worker("websocket://0.0.0.0:2000");
 
 $http_worker->count = 4;
 
-$userid=array();
 
-$http_worker->onConnect=function($connection){
 
+$http_worker->onConnect=function($connection)
+{
+    $jiekou=array();
+    $userid=array();
 };
 $http_worker->onMessage = function($connection, $data)
 {
-    global $http_worker,$userid;
-    foreach($userid as $key=>$value){
-        if(!isset($http_worker->connections[$key])){
-            unset($userid[$key]);
+    global $http_worker,$jiekou,$userid;
+    if(is_array($jiekou)) {
+        foreach ($jiekou as $key => $value) {
+            if (!isset($http_worker->connections[$key])) {
+                unset($jiekou[$key]);
+            }
         }
     }
-    if(!isset($userid[$connection->id])){
-        $data = explode(',', $data);
-        $userid[$connection->id]=array("id"=>$connection->id,"userid"=>$data[0],"name"=>$data[1]);
-    } else {
-        //此处为收到信息后的操作
-        $data=array("name"=>$userid[$connection->id]['name'],"image"=>$data,"time"=>date("Y-m-d H:i:s"));
-        $data=json_encode($data);
-        foreach($userid as $key=>$value){
-            $http_worker->connections[$key]->send($data);
+    if(!isset($jiekou[$connection->id])){
+        $data =json_decode($data);
+        $jiekou[$connection->id]=array("id"=>$connection->id,"name"=>$data[0],"tel"=>$data[1],"email"=>$data[2]);
+        if(is_array($userid)) {
+            $a=0;
+            foreach ($userid as $key => $value) {
+                if ($value['tel'] == $data[1]) {
+                    $a = 1;
+                }
+            }
+            if ($a == 1) {
+                $http_worker->connections[$connection->id]->send("已预约！");
+            }else{
+                $userid[$key+1]=array(
+                    "name"=>$data[0],
+                    "tel"=>$data[1],
+                    "email"=>$data[2]
+                );
+                $http_worker->connections[$connection->id]->send("预约成功！");
+            }
+        }else{
+            $userid[0]=array(
+                "name"=>$data[0],
+                "tel"=>$data[1],
+                "email"=>$data[2]
+            );
+            $http_worker->connections[$connection->id]->send("预约成功！");
         }
-
-
+        print_r($jiekou);
     }
 };
 Worker::runAll();
